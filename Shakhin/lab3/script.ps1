51343509b3bbd6960cc4b3013ac335bf2c9ac011
shakhinn <nik.shakhin@gmail.com>
2020-10-29 00:57:30 +0300

    lab3: done

---
+++ Shakhin/lab3/script.ps1
@@ -0,0 +1,19 @@
+Write-Host "test 1"
+./aisd_lb3 -f tests/test1.txt
+Write-Host "test 2"
+./aisd_lb3  -f tests/test2.txt
+Write-Host "test 3"
+./aisd_lb3  -f tests/test3.txt
+Write-Host "test 4"
+./aisd_lb3  -f tests/test4.txt
+Write-Host "test 5"
+./aisd_lb3  -f tests/test5.txt
+Write-Host "test 6"
+./aisd_lb3  -f tests/test6.txt
+Write-Host "test 7"
+./aisd_lb3  -f tests/test7.txt
+Write-Host "test 8"
+./aisd_lb3 -f tests/test8.txt
+
+
+
---
+++ Shakhin/lab3/src/main.cpp
@@ -0,0 +1,33 @@
+#include "Tree.h"
+#include "InOut.h"
+
+int main (int argc, char** argv){
+    string input = cuinput(argc, argv);
+    if(input.empty()){
+        cout<<"ERROR";
+        return 0;
+    }
+    string copy = string(input);
+    BinaryTree tree = BinaryTree(copy);
+    cout<<input<<endl;
+    if(tree.checkBST()) {
+        cout << "Tree is BST\n";
+        if (argc > 1) {
+            ofstream outfile(optarg, ios::app);
+            outfile << "Tree is BST\n";
+        }
+    }else if(tree.checkPiramid()){
+        cout<<"Tree is a piramid\n";
+        if (argc > 1) {
+            ofstream outfile(optarg, ios::app);
+            outfile << "Tree is a piramid\n";
+        }
+    }else{
+        cout<<"Tree is not BST or Piramid\n";
+        if (argc > 1) {
+            ofstream outfile(optarg, ios::app);
+            outfile << "Tree is not BST or Piramid\n";
+        }
+    }
+    return 0;
+}
